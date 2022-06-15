cbf0498988802317a1adb35fd6c177e9a13b46bb
no-chili <18875854140@163.com>
2022-06-15 11:09:09 +0800

    layout

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,60 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+import { createRouter, createWebHistory } from "vue-router";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: () => import("@/views/HomeView.vue"),
+      meta: {
+        title: "Home",
+      },
+      children: [
+        {
+          path: "dashboard",
+          name: "dashboard",
+          component: () => import("@/views/DashBoardView.vue"),
+          meta: {
+            title: "表盘",
+          },
+        },
+        {
+          path: "vetting",
+          name: "vetitng",
+          component: () => import("@/views/VettingView.vue"),
+          meta: {
+            title: "审核",
+          },
+        },
+        {
+          path: "edit",
+          name: "edit",
+          component: () => import("@/views/EditView.vue"),
+          meta: {
+            title: "编辑",
+          },
+        },
+        {
+          path: "setting",
+          name: "setting",
+          component: () => import("@/views/SettingView.vue"),
+          meta: {
+            title: "设置",
+          },
+        },
+      ],
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+      path: "/login",
+      name: "login",
+      component: () => import("@/views/LoginView.vue"),
+    },
 
-export default router
+    {
+      path: "/:catchAll(.*)",
+      name: "error",
+      component: () => import("@/views/ErrorView.vue"),
+    },
+  ],
+});
+export default router;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,131 @@
-<script setup lang="ts">
-import TheWelcome from '@/components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <div class="common-layout">
+      <el-container>
+        <el-aside width="200">
+          <!-- 菜单 -->
+          <el-menu
+            router
+            :collapse="isCollapse"
+            default-active="1"
+            class="full-height el-menu-vertical-demo"
+            background-color="#05668D"
+            active-text-color="#fff"
+          >
+            <el-menu-item @click="collapse" index="/">
+              <el-icon><img src="@/assets/logo.svg" /></el-icon>
+              <span>User-Chili</span>
+            </el-menu-item>
+            <el-menu-item index="/">
+              <el-icon><House /></el-icon>
+              <span>主页</span>
+            </el-menu-item>
+            <el-sub-menu index="/">
+              <template #title>
+                <el-icon><TrendCharts /></el-icon>
+                <span>图表</span>
+              </template>
+              <el-menu-item index="dashboard">仪表盘</el-menu-item>
+            </el-sub-menu>
+            <el-menu-item index="vetting">
+              <el-icon><Stamp /></el-icon>
+              <span>审批</span>
+            </el-menu-item>
+            <el-menu-item index="edit">
+              <el-icon><Edit /></el-icon>
+              <span>编辑</span>
+            </el-menu-item>
+            <el-menu-item index="setting">
+              <el-icon><setting /></el-icon>
+              <span>设置</span>
+            </el-menu-item>
+          </el-menu>
+        </el-aside>
+        <el-container class="container">
+          <el-header>
+            <el-row justify="space-between" align="middle" class="row-bg">
+              <el-col :span="6">
+                <div>Relax后台模板</div>
+              </el-col>
+              <el-col :span="6">
+                <div class="avatar">
+                  <el-avatar
+                    src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
+                  />
+                  <el-dropdown trigger="click">
+                    <span class="el-dropdown-link">
+                      选项
+                      <el-icon class="el-icon--right">
+                        <arrow-down />
+                      </el-icon>
+                    </span>
+                    <template #dropdown>
+                      <el-dropdown-menu>
+                        <el-dropdown-item>退出</el-dropdown-item>
+                        <el-dropdown-item>切换账号</el-dropdown-item>
+                      </el-dropdown-menu>
+                    </template>
+                  </el-dropdown>
+                </div>
+              </el-col>
+            </el-row>
+          </el-header>
+          <el-page-header :content="title" />
+          <el-main>
+            <el-card class="box-card">
+              <router-view v-slot="{ Component }">
+                <transition name="slide-fade">
+                  <keep-alive>
+                    <component :is="Component" />
+                  </keep-alive>
+                </transition>
+              </router-view>
+            </el-card>
+          </el-main>
+        </el-container>
+      </el-container>
+    </div>
+  </div>
 </template>
+<script setup lang="ts">
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import {
+  TrendCharts,
+  Setting,
+  House,
+  Stamp,
+  Edit,
+  ArrowDown,
+} from "@element-plus/icons-vue";
+import { ref, computed } from "vue";
+import { useRoute } from "vue-router";
+const isCollapse = ref(false);
+function collapse() {
+  isCollapse.value = !isCollapse.value;
+}
+
+function goBack() {
+  return;
+}
+
+const route = useRoute();
+const title = computed(() => {
+  return route.meta.title;
+});
+// const menu = ref({});
+</script>
+<style scoped>
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+  width: 200px;
+  min-height: 400px;
+}
+.el-page-header {
+  padding-left: 20px;
+}
+.avatar {
+  height: 60px;
+  display: flex;
+  justify-content: end;
+  align-items: center;
+}
+</style>
